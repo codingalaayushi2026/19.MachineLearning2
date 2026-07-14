8e4acf371509f02b38dcfd7793224125ebffac08
codingalaayushi2026 <codingalaayushi@gmail.com>
2026-07-15 00:42:06 +0530

    Machine Learnign

---
+++ Lesson6-RecommendationEngine-Part2/Activity.py
@@ -0,0 +1,13 @@
+#IMPORT LIBRARIES AND DATASET
+
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+from math import sqrt
+
+#Importing Dataset
+
+ratings_df = pd.read_csv
+(r'D:\CodingalContent\Courses\AICGM\19.MachineLearning2\Lesson6-RecommendationEngine-Part2\ratings.csv')
+ratings_df.head()
+
